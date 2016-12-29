036647a7c304cfd2babb94aa61ee02290f3af772
coderd <likesimplemail@gmail.com>
2016-12-29 23:23:10 +0800

    Added: default error handler

--- context.go
+++ context.go
@@ -1,8 +1,10 @@
 package rago
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/raframework/rago/raerror"
 	"github.com/raframework/rago/rahttp"
 )
 
@@ -33,8 +35,7 @@ func NewContext(uriPatterns map[rahttp.UriPattern]rahttp.ResourceAndMethod, w ht
 
 func (c *Context) recover() {
 	if err := recover(); err != nil {
-		c.err = err
-		c.errorHandler(err, c.request, c.response)
+		c.handleError(err)
 	}
 }
 
@@ -81,4 +82,47 @@ func (c *Context) Respond() *Context {
 
 func (c *Context) WithErrorHandler(errorHandler ErrorHandler) {
 	c.errorHandler = errorHandler
+}
+
+func (c *Context) handleError(err interface{}) {
+	c.err = err
+	if c.errorHandler != nil {
+		defer func() {
+			if err := recover(); err != nil {
+				c.defaultHandleError(err)
+			}
+		}()
+
+		c.errorHandler(err, c.request, c.response)
+	} else {
+		c.defaultHandleError(err)
+	}
+}
+
+func (c *Context) defaultHandleError(err interface{}) {
+	var statusCode int
+
+	raError, ok := err.(*raerror.RaError)
+	if ok {
+		switch raError.Typ() {
+		case raerror.TypBadRequest:
+			statusCode = 400
+		case raerror.TypNotFound:
+			statusCode = 404
+		case raerror.TypMethodNotAllowed:
+			statusCode = 405
+		case raerror.TypUnsupportedMediaType:
+			statusCode = 415
+		default:
+			statusCode = 500
+		}
+	} else {
+		statusCode = 500
+	}
+
+	if statusCode == 500 {
+		log.Println("[Error] rago: ", err)
+	}
+
+	c.response.WithStatus(statusCode)
 }
